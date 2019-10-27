76ed6deafb94df5014b34bb1ea18e45aa40a9034
Dominik Palacz <palacz.dominik@gmail.com>
2019-10-27 15:19:58 +0100

    File System

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-// /*jshint esversion: 6 */
-// // /**
-// //  * To start use:
-// //  * node app.js */
+// // /*jshint esversion: 6 */
+// // // /**
+// // //  * To start use:
+// // //  * node app.js */
 
-// // // const sayHello = name => console.log(`Hi ${name}`);
-// // // sayHello("Dominik");
-// // // console.log(module);
+// // // // const sayHello = name => console.log(`Hi ${name}`);
+// // // // sayHello("Dominik");
+// // // // console.log(module);
 
-// // /** Add new Module */
+// // // /** Add new Module */
 
-// // /** Load module */
-// // const log = require("./logger");
+// // // /** Load module */
+// // // const log = require("./logger");
 
-// // // console.log("module :", module);
-// // // console.log(logger);
+// // // // console.log("module :", module);
+// // // // console.log(logger);
 
-// // // logger.log("message Dominik");
-// // log("message Dominik log");
+// // // // logger.log("message Dominik");
+// // // log("message Dominik log");
 
+// // const path = require('path');
 
-// const path = require('path');
+// // const pathObj = path.parse(__filename);
 
-// const pathObj = path.parse(__filename);
+// // console.log('pathObj :', pathObj);
 
-// console.log('pathObj :', pathObj);
+// const os = require('os');
 
-const os = require('os');
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
 
-const totalMemory = os.totalmem();
-const freeMemory = os.freemem();
+// console.log('totalMemory :', totalMemory);
+// console.log('freeMemory  :', freeMemory);
 
-console.log('totalMemory :', totalMemory);
-console.log('freeMemory  :', freeMemory);
+const fs = require("fs");
+
+// const files = fs.readdirSync("./");
+// console.log("files :", files);
+
+// fs.readdir("./", (err, files) => {
+//   err ? console.log("err  :", err) : console.log("files :", files);
+// });
+
+fs.readdir("createErr", (err, files) => {
+  err ? console.log("err  :", err) : console.log("files :", files);
+});
