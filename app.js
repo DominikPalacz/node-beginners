d3034721c901f95374bed70e951b7c777b0b3b4b
Dominik Palacz <palacz.dominik@gmail.com>
2019-10-27 16:45:15 +0100

    Use first HTTP

--- app.js
+++ app.js
@@ -63,3 +63,22 @@
 // // emitter.emit("logging", { data: message });
 
 // // Making a noise, produce - signalling
+
+// server.on("connection", socket => console.log("New connection..."));
+const http = require("http");
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    res.write("Hello world from server");
+    res.end();
+  }
+
+  if (req.url === "/api/numbers") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+server.listen(5000);
+
+console.log("Listening on port 5000 :");
