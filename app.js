f05bd98be9e65a3e8cfc1fa1714e4f7f294e65d7
Dominik Palacz <palacz.dominik@gmail.com>
2019-10-27 15:11:15 +0100

    OS

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-/*jshint esversion: 6 */
-// /**
-//  * To start use:
-//  * node app.js */
+// /*jshint esversion: 6 */
+// // /**
+// //  * To start use:
+// //  * node app.js */
 
-// // const sayHello = name => console.log(`Hi ${name}`);
-// // sayHello("Dominik");
-// // console.log(module);
+// // // const sayHello = name => console.log(`Hi ${name}`);
+// // // sayHello("Dominik");
+// // // console.log(module);
 
-// /** Add new Module */
+// // /** Add new Module */
 
-// /** Load module */
-// const log = require("./logger");
+// // /** Load module */
+// // const log = require("./logger");
 
-// // console.log("module :", module);
-// // console.log(logger);
+// // // console.log("module :", module);
+// // // console.log(logger);
 
-// // logger.log("message Dominik");
-// log("message Dominik log");
+// // // logger.log("message Dominik");
+// // log("message Dominik log");
 
 
-const path = require('path');
+// const path = require('path');
 
-const pathObj = path.parse(__filename);
+// const pathObj = path.parse(__filename);
 
-console.log('pathObj :', pathObj);
+// console.log('pathObj :', pathObj);
+
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log('totalMemory :', totalMemory);
+console.log('freeMemory  :', freeMemory);
