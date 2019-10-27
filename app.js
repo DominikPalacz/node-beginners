968e16b7d3632bb6256dfcc33d18b2edf2a08a33
Dominik Palacz <palacz.dominik@gmail.com>
2019-10-27 16:17:28 +0100

    Clean code

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-// // // /*jshint esversion: 6 */
-// // // // /**
-// // // //  * To start use:
-// // // //  * node app.js */
+// // // // /*jshint esversion: 6 */
+// // // // // /**
+// // // // //  * To start use:
+// // // // //  * node app.js */
 
-// // // // // const sayHello = name => console.log(`Hi ${name}`);
-// // // // // sayHello("Dominik");
-// // // // // console.log(module);
+// // // // // // const sayHello = name => console.log(`Hi ${name}`);
+// // // // // // sayHello("Dominik");
+// // // // // // console.log(module);
 
-// // // // /** Add new Module */
+// // // // // /** Add new Module */
 
-// // // // /** Load module */
-// // // // const log = require("./logger");
+// // // // // /** Load module */
+// // // // // const log = require("./logger");
 
-// // // // // console.log("module :", module);
-// // // // // console.log(logger);
+// // // // // // console.log("module :", module);
+// // // // // // console.log(logger);
 
-// // // // // logger.log("message Dominik");
-// // // // log("message Dominik log");
+// // // // // // logger.log("message Dominik");
+// // // // // log("message Dominik log");
 
-// // // const path = require('path');
+// // // // const path = require('path');
 
-// // // const pathObj = path.parse(__filename);
+// // // // const pathObj = path.parse(__filename);
 
-// // // console.log('pathObj :', pathObj);
+// // // // console.log('pathObj :', pathObj);
 
-// // const os = require('os');
+// // // const os = require('os');
 
-// // const totalMemory = os.totalmem();
-// // const freeMemory = os.freemem();
+// // // const totalMemory = os.totalmem();
+// // // const freeMemory = os.freemem();
 
-// // console.log('totalMemory :', totalMemory);
-// // console.log('freeMemory  :', freeMemory);
+// // // console.log('totalMemory :', totalMemory);
+// // // console.log('freeMemory  :', freeMemory);
 
-// const fs = require("fs");
+// // const fs = require("fs");
 
-// // const files = fs.readdirSync("./");
-// // console.log("files :", files);
+// // // const files = fs.readdirSync("./");
+// // // console.log("files :", files);
 
-// // fs.readdir("./", (err, files) => {
+// // // fs.readdir("./", (err, files) => {
+// // //   err ? console.log("err  :", err) : console.log("files :", files);
+// // // });
+
+// // fs.readdir("createErr", (err, files) => {
 // //   err ? console.log("err  :", err) : console.log("files :", files);
 // // });
 
-// fs.readdir("createErr", (err, files) => {
-//   err ? console.log("err  :", err) : console.log("files :", files);
-// });
+// // CLASS
+// const EventEmitter = require("events");
+// // const emitter = new EventEmitter();
 
-// CLASS
-const EventEmitter = require("events");
-// const emitter = new EventEmitter();
+// const Logger = require("./logger");
+// const logger = new Logger();
+// let message = "Try some message from app.js";
 
-const Logger = require("./logger");
-const logger = new Logger();
-let message = "Try some message from app.js";
+// // Register a listener
+// logger.on("messageLogged", arg => console.log("Listener called", arg)); // e, eventArg
+// logger.on("logging", ({ data }) => console.warn("Logging data:", data));
 
-// Register a listener
-logger.on("messageLogged", arg => console.log("Listener called", arg)); // e, eventArg
-logger.on("logging", ({ data }) => console.warn("Logging data:", data));
+// logger.log(message);
+// // // Raise an event
+// // emitter.emit("messageLogged", { id: 1, url: "http://" });
+// // emitter.emit("logging", { data: message });
 
-logger.log(message);
-// // Raise an event
-// emitter.emit("messageLogged", { id: 1, url: "http://" });
-// emitter.emit("logging", { data: message });
-
-// Making a noise, produce - signalling
+// // Making a noise, produce - signalling
--- logger.js
+++ logger.js
@@ -1,31 +1,31 @@
-/** Module Wrapper Function */
+// /** Module Wrapper Function */
 
-// (function(exports, require, module, __filename, __dirname) {
-// var x =;
-/** Creating a Module */
-// console.log("__filename :", __filename);
-// console.log("__dirname :", __dirname);
+// // (function(exports, require, module, __filename, __dirname) {
+// // var x =;
+// /** Creating a Module */
+// // console.log("__filename :", __filename);
+// // console.log("__dirname :", __dirname);
 
-const EventEmitter = require("events");
-// const emitter = new EventEmitter();
-let message = "Try some message";
+// const EventEmitter = require("events");
+// // const emitter = new EventEmitter();
+// let message = "Try some message";
 
-const url = "http://myLogger.io/log";
+// const url = "http://myLogger.io/log";
 
-class Logger extends EventEmitter {
-  log = message => {
-    // Send an HTTP request
-    console.log(message);
+// class Logger extends EventEmitter {
+//   log = message => {
+//     // Send an HTTP request
+//     console.log(message);
 
-    // Raise an event
-    this.emit("messageLogged", { id: 1, url: "http://" });
-    this.emit("logging", { data: message });
-  };
-}
+//     // Raise an event
+//     this.emit("messageLogged", { id: 1, url: "http://" });
+//     this.emit("logging", { data: message });
+//   };
+// }
 
-// module.exports.log = log; // object
-// module.exports = log; // single function
-module.exports = Logger; // Class
+// // module.exports.log = log; // object
+// // module.exports = log; // single function
+// module.exports = Logger; // Class
 
-// module.exports.endPoint = url; // ! Keep the URL private!
-// });
+// // module.exports.endPoint = url; // ! Keep the URL private!
+// // });
